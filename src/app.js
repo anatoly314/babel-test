71e18dc41394b4343e0d251af7c54a6f82fef72e
Anatoly Tarnavsky <anatolyt@gmail.com>
2023-01-04 21:35:29 +0200

    Simplified example

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import MyModule from "./module.js";
 
-const start = async () => {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-
-    const module = await import(__dirname + "/module.js");
-    const initialized = new module.default();
-    const description = await initialized.getDescription();
-    console.log(description);
-}
-
-await start();
+const initializedModule = new MyModule();
+const description = initializedModule.getDescription();
+console.log(description);
--- src/module.js
+++ src/module.js
@@ -2,7 +2,7 @@ import {test} from "./decorators.js";
 
 export default class MyModule {
     @test(true)
-    async getDescription () {
+    getDescription () {
         return "MyModule Test12";
     }
 }
